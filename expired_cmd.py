55918bd4346f3fba153638a9873d6401b3f89229
Zhuoqian Zhou <ben02060846@gmail.com>
2023-10-26 19:52:41 +0800

    modified:   expired_cmd.py

--- expired_cmd.py
+++ expired_cmd.py
@@ -1,21 +1,13 @@
-# -*- coding: utf-8 -*-
 import pickle
-import abc
 import os
 
-class ExpiredCmd(metaclass=abc.ABCMeta):
-    
+class ExpiredCmd:
+
     CMD_CACHE_FILE = "cmd_cache.pk"
 
     def __init__(self):
         self.expired_cmds = set()
-        
-    def add_cmd_to_expired_cmds(self, cmd):
-        key = self.generate_expired_cmd_key(cmd)
-        self.expired_cmds.add(key)
-
-        with open(self.get_cmd_cache_file_nm(), "wb") as f:
-            pickle.dump(self.expired_cmds, f)
+        self.load_expired_cmd_cache()
 
     @staticmethod
     def generate_expired_cmd_key(cmd):
@@ -26,18 +18,24 @@ class ExpiredCmd(metaclass=abc.ABCMeta):
             cmd["action"],
             cmd["msg_id"],
         )
-        
     def get_cmd_cache_file_nm(self):
-        _new_name = '%s' % (self.CMD_CACHE_FILE)
-        return _new_name
-     
+        return self.CMD_CACHE_FILE
+
+    def add_cmd_to_expired_cmds(self, cmd):
+        key = self.generate_expired_cmd_key(cmd)
+        self.expired_cmds.add(key)
+        self.save_expired_cmd_cache()
+
     def is_cmd_expired(self, cmd):
         key = self.generate_expired_cmd_key(cmd)
         return key in self.expired_cmds
-    
-    def load_expired_cmd_cache(self):
-        _new_name = self.get_cmd_cache_file_nm()
-        if os.path.exists(_new_name):
-            with open(_new_name, "rb") as f:
-                self.expired_cmds = pickle.load(f) 
 
+    def load_expired_cmd_cache(self):
+        cache_file = self.get_cmd_cache_file_nm()
+        if os.path.exists(cache_file):
+            with open(cache_file, "rb") as f:
+                self.expired_cmds = pickle.load(f)
+
+    def save_expired_cmd_cache(self):
+        with open(self.get_cmd_cache_file_nm(), "wb") as f:
+            pickle.dump(self.expired_cmds, f)
