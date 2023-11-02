222cf0f5e8b66ddfaf0a88cd5e023a92021675f0
Zhuoqian Zhou <zhuoqian.zhou@dalveytech.com>
2023-11-03 04:08:24 +0800

    add socket.io

--- xqfollower/xq_ws_mgr.py
+++ xqfollower/xq_ws_mgr.py
@@ -1,8 +1,8 @@
 import time
-import asyncio
-import websockets
+import socketio
 
 from .log import logger 
+from .time_utils import is_off_trading_hour
 
 class XueQiuWebsocketManager:
     
@@ -10,25 +10,42 @@ class XueQiuWebsocketManager:
         self.target = target
         self.action = action
         logger.info("openning ws...")
-        asyncio.get_event_loop().run_until_complete(self.ws_handler)
-        asyncio.get_event_loop().run_forever()
+        # 创建 Socket.IO 客户端
+        self.sio = socketio.Client(reconnection=True, reconnection_attempts=999, reconnection_delay=5)
+        self.sio.connect('http://localhost:3000')
         
-    async def ws_handler(self):
-        while True:
-            try:
-                async with websockets.connect('ws://127.0.0.1:4000') as websocket:
-                    logger.info("ws connected...")
-                    while True:
-                        try:
-                            body_content = await websocket.recv()
-                            getattr(self.target, self.action)(body_content)
-                        except websockets.WebSocketException.ConnectionClosed as e:
-                            await websocket.close()  # Close the WebSocket connection
-                            logger.warning("WebSocket disconnected")
-                            break
-                        except Exception as e:
-                            logger.warning(e)
-                            break
-            except Exception as e:
-                logger.warning(e)
-                time.sleep(5)
+        # 当连接建立时的事件处理函数
+        @self.sio.on('connect')
+        def on_connect():
+            logger.info('已连接到服务器')
+
+        # 当收到广播消息时的事件处理函数
+        @self.sio.on('broadcast_message')
+        def on_broadcast_message(data):
+            logger.info(f'收到广播消息：{data}')
+            if is_off_trading_hour():
+                self.sio.emit('special_message', data)
+            else:
+                getattr(self.target, self.action)(data)
+                
+        # 当收到特定消息时的事件处理函数
+        @self.sio.on('consumed_message')
+        def on_consumed_message(data):
+            logger.info(f'消费特定消息：{data}')
+            getattr(self.target, self.action)(data)
+    
+    # 发送消息到服务端
+    def send_message(self,message):
+        self.sio.emit('message', message)
+
+    # 发送特定消息到服务端
+    def send_special_message(self,message):
+        self.sio.emit('special_message', message)
+
+    # 查询特定消息
+    def query_special_messages(self):
+        self.sio.emit('query_special_messages')
+
+    # 消费特定消息
+    def consume_special_messages(self):
+        self.sio.emit('consume_special_messages')
